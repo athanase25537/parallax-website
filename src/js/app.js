a9d302c8b53d73ab21192c0d2a137da5bb187a4c
ANDRIAMASY Athanase Marc <andriamasy@kali.athanase.org>
2024-03-18 17:31:59 +0300

    create and add svt animation

--- src/js/app.js
+++ src/js/app.js
@@ -15,3 +15,18 @@ document.body.onscroll = () => {
 
     title.style.marginTop = scrollY + 'px';
 }
+
+let path = document.querySelector('path');
+let pathLength = path.getTotalLength();
+path.style.strokeDasharray = pathLength+ ' ' + pathLength;
+path.style.strokeDashoffset = pathLength;
+
+addEventListener('scroll', () => {
+    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+
+    // Length to offset the dashes
+    var drawLength = pathLength * scrollPercentage;
+
+    // Draw in reverse
+    path.style.strokeDashoffset = pathLength - drawLength;
+})
